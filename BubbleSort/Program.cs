07dd7489c9d4dfb65cae0a072a0b2e54093ac4a3
Coolsnake8 <foxtailrb@gmail.com>
2022-10-22 21:19:35 -0600

    Added debugging files from IDE and setup skeleton for BubbleSort program.

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Bubble
 {
@@ -6,9 +7,32 @@ namespace Bubble
     {
         static void Main(string[] args)
         {
+            var watch = new System.Diagnostics.Stopwatch();
+
             int[] inputIntegers = {74, 87, 83, 48, 34, 53, 86, 65, 84, 63, 77, 12, 85, 99, 15, 51, 0, 54, 26, 60};
 
-            
+            watch.Start();
+
+
+
+            watch.Stop();
+
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+            Console.WriteLine("Sorted List: ");
+        }
+
+        public int[] BubbleSort(int[] inputArray)
+        {
+            bool isSorted = false;
+
+            while (isSorted == false)
+            {
+                for (int counter = 0 ; counter < inputArray.Length - 1 ; counter++)
+                {
+
+                }
+            }
         }
     }
 }
