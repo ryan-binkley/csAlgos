1fbbd136fd61a6b95fe599ea3710ef22bbad8306
Coolsnake8 <foxtailrb@gmail.com>
2022-10-22 22:11:10 -0600

    Code is now documented for later understanding.

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -7,42 +7,53 @@ namespace Bubble
     {
         static void Main(string[] args)
         {
+            // Diagnostics has a built in timer for recording execution time
             var watch = new System.Diagnostics.Stopwatch();
 
+            // Generate an unsorted array of x numbers
             int[] inputIntegers = GenerateRandomIntegerArray(100000);
 
+            // Begin recording runtime
             watch.Start();
 
             BubbleSort(inputIntegers);
 
+            // End recording runtime
             watch.Stop();
 
             Console.WriteLine("Sorted List: ");
 
             PrintSortedArray(BubbleSort(inputIntegers));
 
+            // Print to the console the runtime in milliseconds
             Console.WriteLine($"\nExecution Time: {watch.ElapsedMilliseconds} ms");
         }
 
         public static int[] BubbleSort(int[] inputArray)
         {
+            // Create a buffer to move around the one int we are sorting
             int tempInt;
 
+            // Iterate through the array for every single integer in the array
             for (int counter1 = 0 ; counter1 < inputArray.Length ; counter1++)
             {
+                // Iterate and check each integers next neighbor to see if a swap is necessary
                 for (int counter2 = 0 ; counter2 < inputArray.Length - 1 ; counter2++)
                 {
                     if (inputArray[counter2] > inputArray[counter2 + 1])
                     {
+                        // If swap is necessary, put the neighboring integer into the buffer
                         tempInt = inputArray[counter2 + 1];
 
+                        // Set the neighbor to the current integer
                         inputArray[counter2 + 1] = inputArray[counter2];
 
+                        // Set original integer to the integer in the buffer
                         inputArray[counter2] = tempInt;
                     } 
                 }
             }
-
+            // Return the sorted inputArray
             return inputArray;
         }
 
