0991c3bc7c1276d238b57019a626bbcd1fda019b
Ryan Binkley <60923299+ryan-binkley@users.noreply.github.com>
2022-10-26 10:55:18 -0600

    Copied Sort boilerplate to CycleSort 'Main'.

--- CycleSort/Program.cs
+++ CycleSort/Program.cs
@@ -7,7 +7,26 @@ namespace CycleSort
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello and goodbye.");
+			 // Diagnostics has a built in timer for recording execution time
+            var watch = new System.Diagnostics.Stopwatch();
+
+            // Generate an unsorted array of x numbers
+            int[] inputIntegers = GenerateRandomIntegerArray(10000);
+
+            // Begin recording runtime
+            watch.Start();
+
+            CycleSort(inputIntegers);
+
+            // End recording runtime
+            watch.Stop();
+
+            Console.WriteLine("Sorted List: ");
+
+            PrintSortedArray(CycleSort(inputIntegers));
+
+            // Print to the console the runtime in milliseconds
+            Console.WriteLine($"\nExecution Time: {watch.ElapsedMilliseconds} ms");
 		}
 	}
 }
